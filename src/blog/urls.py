550426e137285742d65a0192136634634ccbdefe
Masnekov Ivan <masnekovivan@gmail.com>
2022-02-22 11:04:58 +0300

    Fix blog page. Now it show recent 4 post in descending order. Search bar is working. Latest posts in rigth side of the Blog working, and shows three recent posts. Categories panel in blof working, and shows how many posts in specific category

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -3,15 +3,18 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from posts.views import index, blog, post
+from posts.views import index, blog, post, search
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
-    path('blog/', blog),
-    path('post/', post),
+    path('blog/', blog, name='post_list'),
+    path('search/', search, name='search'),
+    path('post/<id>/', post, name='post_detail'),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
--- src/posts/models.py
+++ src/posts/models.py
@@ -2,6 +2,7 @@ from sqlite3 import Timestamp
 from tkinter import CASCADE
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 User = get_user_model()
 
@@ -10,14 +11,12 @@ class Author(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_picture = models.ImageField()
 
-
     def __str__(self):
         return self.user.username
 
 
 class Category(models.Model):
     title = models.CharField(max_length=20)
-
 
     def __str__(self):
         return self.title
@@ -28,11 +27,14 @@ class Post(models.Model):
     overview = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     comment_count = models.IntegerField(default=0)
+    view_count = models.IntegerField(default=0)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     thumbnail = models.ImageField()
     categories = models.ManyToManyField(Category)
     featured = models.BooleanField()
 
-
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={"id": self.id})
